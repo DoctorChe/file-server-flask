02de39971dbe35f2673c58dd76c11696986646f4
DoctorChe <duncan.reg@yandex.ru>
2023-05-24 14:05:01 +0300

    setup database

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -7,3 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
     SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
